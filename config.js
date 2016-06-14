b86514d1501a3202e7cbdc152e62d2f337e00312
yuthset <syuth89@gmail.com>
2016-06-14 16:10:54 +0700

    initial my UserStory app into gitHub

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+	"database" : "mongodb://root:123abc@ds011024.mlab.com:11024/angularapp",
+	"port" : process.env.PORT || 3000,
+	"secretKey" : "YourSecretKey"
+}
---
+++ public/app/app.js
@@ -0,0 +1,4 @@
+angular.module('myApp',['appRoutes', 'mainCtrl', 'authService', 'userCtrl', 'userService', 'storyService', 'storyCtrl', 'reverseDirective'])
+	.config(function($httpProvider) {
+		$httpProvider.interceptors.push('AuthInterceptor');
+	})
